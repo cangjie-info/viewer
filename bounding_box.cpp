3960bfd3569a716f47cda9a92113abced56b3c2b
Adam Smith <adam@cangjie.info>
2011-07-14 10:15:59 -0400

    Bounding box with new isNull member. Tested.

---
+++ bounding_box.cpp
@@ -0,0 +1,32 @@
+#include <QDebug>
+#include "bounding_box.h"
+
+BoundingBox::BoundingBox(QPoint point1, QPoint point2, int angle, bool null) 
+	: QRect(point1, point2), rotation(angle), isNull(null)
+{
+	//constructor
+}
+
+int BoundingBox::getRotation() const
+{
+	return rotation;
+}
+
+void BoundingBox::setNull(bool null)
+{
+	isNull = null;
+}
+
+bool BoundingBox::boxIsNull() const
+{
+	return isNull;
+}
+
+void BoundingBox::report() const
+{
+	qDebug() << "REPORT FOR BoundingBox";
+	qDebug() << "x1 = " << x() << ", y1 = " << y();
+	qDebug() << "x2 = " << x()+width()-1 << ", y2 = " << y()+height()-1;
+	qDebug() << "rotation = " << getRotation();
+	qDebug() << "box is" << (isNull ? "" : "not") << "null.";
+}
---
+++ bounding_box.h
@@ -0,0 +1,30 @@
+//class for bounding box around inscription or glyph in image
+//TODO could rotation not just be made a public member??
+
+#ifndef BOUNDING_BOX_H
+#define BOUNDING_BOX_H
+
+#include <QRect>
+
+class BoundingBox : public QRect
+{
+	//	Q_OBJECT  -  this generated an error 
+public:
+	BoundingBox(QPoint point1, QPoint point2, int angle, bool null); //constructor
+	int getRotation() const;		//returns rotation
+	void setNull(bool null); //sets boxIsNull to isNull
+	bool boxIsNull() const; //returns value of boxIsNull
+	void report() const;
+private:
+	int rotation;		//rotation of image when rectangle was set
+							//center of rotation depends on image !!
+	bool isNull;	//if a box is null, it simply occupies a position in
+			//a list of boxes; values of QRect and rotation are just junk.
+			//This if for cases where there is a graph in the transcription but 
+			//nothing visible in the surface image to have a bbox drawn around.
+			//the same functionality could have been achieved by setting QRect to zero
+			//but this seems simpler. It also means that null-ing a bbox is undoable, since 
+			//the value of QRect will preserve.
+};
+	
+#endif
