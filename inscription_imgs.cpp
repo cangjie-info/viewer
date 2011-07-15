cdac5023795a78659b5863ca162e9b2eca2be251
Adam Smith <adam@cangjie.info>
2011-07-15 13:38:45 -0400

    InscripitonImgs class added. This is-a BoxList, and is-a BoundingBox. The BoxList holds the graph boxes for the inscription, and the BoundingBox holds is the inscription box. note that all transcription functionality is kept outside this class, which has no access to it.

---
+++ inscription_imgs.cpp
@@ -0,0 +1,17 @@
+#include <QDebug>
+#include "inscription_imgs.h"
+
+InscriptionImgs::InscriptionImgs(BoundingBox inscrBox)
+	: BoundingBox(inscrBox)
+{
+	//constructor
+}
+
+void InscriptionImgs::report() const
+{
+	qDebug() << "*** REPORT FOR InscriptionImgs ***";
+	BoundingBox::report();
+	BoxList::report();
+	qDebug() << "*** END REPORT FOR Inscription ***";
+}
+
---
+++ inscription_imgs.h
@@ -0,0 +1,32 @@
+#ifndef INSCRIPTION_IMGS_H
+#define INSCRIPTION_IMGS_H
+
+//consists of a list inherited from BoxList (is-a BoxList).
+//also has a (or rather "is-a")  bounding box of its own corresponding to the bbox of the inscription
+
+//an instance of the class corresponds to one row of ec.inscriptions
+//together with the the associated entries from ec.inscriptionGraphs.
+
+//transcriptional info is handled in a different class
+//no changes to this class must change any representation of 
+//the transcription
+
+//inherits (is-a) BoxList for list of graph bounding boxes.
+//inherits (is-a) BoundingBox.
+//NOTE this should have been done with composition rather than inheritance,
+//but there we are...
+
+#include "graph_transcription.h"
+#include "bounding_box.h"
+#include "box_list.h"
+#include <QList>
+
+class InscriptionImgs : public BoxList, public BoundingBox
+{
+public:
+	InscriptionImgs(BoundingBox inscrBox); //constructor
+	void report() const;
+private:
+};
+
+#endif
