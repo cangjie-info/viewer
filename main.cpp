9f7eaa74ebb8948001d9aed2aed876e72f208cc7
Adam Smith <adam@cangjie.info>
2011-07-21 01:10:08 -0400

    removal of commented junk from main.cpp

--- main.cpp
+++ main.cpp
@@ -12,41 +12,5 @@ int main(int argc, char** argv)
 	
 	Viewer viewer;
 	viewer.show();
-/*
-//test DbHandler class
-	DbHandler dbHandler;
-	if(!dbHandler.connect())
-	{
-		return 1;
-	}
-	else 
-	{
-		qDebug() << "successfully connected to db.";
-	}
-
-	dbHandler.setCorpus();
-	dbHandler.nextSurface();
-	dbHandler.nextSurface();
-	dbHandler.nextSurface();
-	dbHandler.nextSurface();
-	dbHandler.previousSurface();
-	qDebug() << "position in corpus =" << dbHandler.getPositionInCorpus()
-		<< "/" << dbHandler.getCorpusSize();
-
-	SurfaceImgs surf;
-	dbHandler.readSurface(surf);
-	surf.report();
-
-//test SurfaceImgs class
-	BoundingBox surfBox(QPoint(10, 10), QPoint(1000, 1000), 0, false);
-	surfBox.setBox(QPoint(333, 333), QPoint(666, 666), 180, true);
-	SurfaceImgs mySurface(surfBox);
-	mySurface.insertInscr(BoundingBox(QPoint(30, 30), QPoint(70, 70), 30, false), 0);
-	InscriptionImgs* ptrInscr = mySurface.ptrInscrAt(0);
-	ptrInscr->BoundingBox::toggleNull();
-	ptrInscr->insertBox(BoundingBox(QPoint(40, 40), QPoint(50, 50), 0, false), 0);
-	ptrInscr->insertBox(BoundingBox(QPoint(55, 55), QPoint(65, 65), 30, false), 0);
-	mySurface.report();
-*/
 	return app.exec(); 
 }
